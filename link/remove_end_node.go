aa3f08530218959396c2dd29b9b972803dec50b4
zhangsan <81933396@qq.com>
2023-07-28 17:55:41 +0800

    链表删除倒数第Nå个节点Demo

---
+++ link/remove_end_node.go
@@ -0,0 +1,24 @@
+package link
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func removeNthFromEnd(head *ListNode, n int) *ListNode {
+	result := &ListNode{}
+	result.Next = head
+	var pre *ListNode
+	cur := result
+	i := 1
+	for head != nil {
+		if i >= n {
+			pre = cur
+			cur = cur.Next
+		}
+		head = head.Next
+		i++
+	}
+	pre.Next = pre.Next.Next
+	return result.Next
+}
